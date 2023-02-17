ac91a00000e189c9eda7dc114bc8889f9ece4ae1
Emily Nelson <evnelson2021@gmail.com>
2023-02-17 11:40:32 -0600

    added length to the fields requiring urls

--- rareapi/models/author.py
+++ rareapi/models/author.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 
 class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.CharField(max_length=50)
-    profile_image_url = models.CharField(max_length=100)
+    bio = models.CharField(max_length=500)
+    profile_image_url = models.CharField(max_length=250)
 
 
     @property
--- rareapi/models/reaction.py
+++ rareapi/models/reaction.py
@@ -1,4 +1,4 @@
 from django.db import models
 
 class Reaction(models.Model):
-    emoji = models.CharField(max_length=50)
+    emoji = models.CharField(max_length=250)
