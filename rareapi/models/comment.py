cf760d985b63fb763fe1350828c6663fb1aceec2
Lauren Hanson <lohanson33@gmail.com>
2023-02-17 11:27:09 -0600

    category and comment models added

---
+++ rareapi/models/comment.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Comment(models.Model):
+
+    author = models.ForeignKey("Author", null=True, blank=True, on_delete=models.CASCADE, related_name='author_comment')
+    post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name='post_comment')
+    content = models.CharField(max_length=300)
+    
