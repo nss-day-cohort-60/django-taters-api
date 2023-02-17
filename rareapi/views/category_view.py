f9d918f84ca1ffdf65561c535cec43956bf93dc6
Lauren Hanson <lohanson33@gmail.com>
2023-02-17 14:31:47 -0600

    new main

---
+++ rareapi/views/category_view.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from rareapi.models import Category
+
+
+class CategoryView(ViewSet):
+
+    def list(self, request):
+
+        categories = Category.objects.all()
+        serialized = CategorySerializer(categories, many=True)
+        return Response(serialized.data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, pk=None):
+
+        category = Category.objects.get(pk=pk)
+        serialized = CategorySerializer(category, context={'request': request})
+        return Response(serialized.data, status=status.HTTP_200_OK)
+    
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ('id', 'label', )
