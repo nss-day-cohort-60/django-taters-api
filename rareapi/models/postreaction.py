7321f4b77d277da1026ab114af80ca8008cf743a
Jessica Meeker <113938036+jesmeeker@users.noreply.github.com>
2023-02-17 13:19:32 -0600

    stash

--- rareapi/models/postreaction.py
+++ rareapi/models/postreaction.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class PostReaction(models.Model):
     post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name='posts')
-    tag = models.ForeignKey("Tag", on_delete=models.CASCADE, related_name='post_tags')
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name='user_reaction')
     reaction = models.ForeignKey("Reaction", on_delete=models.SET_NULL, related_name='post_reaction')
