898670ad9a582a7f0d25241663820dfad805b130
Nick Valdez <nicholastvaldez@gmail.com>
2023-02-20 10:41:00 -0600

    commit to get new main

--- rare/urls.py
+++ rare/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
+from rest_framework import routers
+from rareapi.views import PostView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'posts', PostView, 'post')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+
 ]
---
+++ rareapi/views/post.py
@@ -0,0 +1,40 @@
+"""View module for handling requests about posts"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from rareapi.models import Post
+
+
+class PostView(ViewSet):
+    """Rare post view"""
+
+    def retrieve(self, request, pk):
+        """Handle GET requests for events
+
+        Returns:
+            Response -- JSON serialized events
+        """
+        post = Post.objects.get(pk=pk)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+    def list(self, request):
+        """Handle GET requests to get all posts
+
+        Returns:
+            Response -- JSON serialized list of posts
+        """
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+
+
+class PostSerializer(serializers.ModelSerializer):
+    """JSON serializer for posts
+    """
+    class Meta:
+        model = Post
+        fields = ('id', 'author', 'category',
+                  'title', 'publication_date', 'image_url', 'content', 'approved',
+                  'reactions', 'tags')
