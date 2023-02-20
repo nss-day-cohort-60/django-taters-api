6d43264d76e22dd5cf54c01a8733e9053eafda72
Jessica Meeker <113938036+jesmeeker@users.noreply.github.com>
2023-02-20 11:11:06 -0600

    login and register updated with encrypted token

--- rare/urls.py
+++ rare/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rareapi.views import register_user, login_user
+from rest_framework import routers
+
+
+router = routers.DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('register', register_user),
+    path('login', login_user),
+    path('admin/', admin.site.urls)
 ]
