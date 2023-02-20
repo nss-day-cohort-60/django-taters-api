f1a60a6310b87dd9ded20d104cf603f8b6a321d2
Lauren Hanson <lohanson33@gmail.com>
2023-02-20 10:36:26 -0600

    stash changes

--- rare/urls.py
+++ rare/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from rareapi.views import CategoryView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'categories', CategoryView, 'category')
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
